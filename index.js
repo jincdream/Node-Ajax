904706eac4dcac4cae87818e76982b1b188d4bb5
jincdream <275727449@qq.com>
2014-07-30 14:22:11 +0800

    添加注释

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = require('./lib/node-ajax.js');
+/*在子进程开启 http 服务器*/
 module.exports.server = function(file,port){
 	if(!port && !process.env.PORT)process.env.PORT = 3000;
 	else process.env.PORT = port;
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 var $ = require('../index.js');
+/*在子进程开启 http 服务器*/
 $.server('./test/http.js',80);
+/*模拟发送请求*/
 $('/').get(function(data){
 	console.log(data);
 });
